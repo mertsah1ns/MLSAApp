c12026d27ff3ad8537d57bd6590bab14a53b2cbc
Ceyda Selamet <118660195+ceydaselamet@users.noreply.github.com>
2023-12-24 13:52:49 +0300

    Update RegisterDto.cs
    IsTeamMember added

--- Core/DTOs/RegisterDto.cs
+++ Core/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,8 @@ namespace Core.DTOs
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        
+        [DefaultValue(false)]
+        public bool IsTeamMember { get; set; }
     }
 }
